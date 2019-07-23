b4253a83692dc2bf35c883977df9f99331be6abd
whamer100 <whammer100@aol.com>
2019-07-23 02:27:03 -0700

    Image stuff working
    Todo: get it to convert from image to ascii

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 extern crate argparse;
+extern crate image;
 
 use argparse::{ArgumentParser, StoreTrue, Store};
+//use image::{save_buffer_with_format, ColorType};
 
 fn main() {
     let mut verbose = false;
     let mut output = "output.txt".to_string();
     let mut input = "input.txt".to_string();
+    let mut do_resize: bool = false;
     let mut d_temp: String = "".to_string();
-    let mut dimensions = vec![0; 2];
+    let mut dimensions: Vec<u32> = vec![0; 2];
     {  // this block limits scope of borrows by ap.refer() method
         let mut ap = ArgumentParser::new();
         ap.set_description("Converts an image to ascii art.");
@@ -16,7 +19,7 @@ fn main() {
             "Increases verbosity");
         ap.refer(&mut input)
             .add_option(&["-i", "--input"], Store,
-            "Input image to convert")
+            "Input image to convert (Currently only supporting PNG)")
             .required();
         ap.refer(&mut output)
             .add_option(&["-o", "--output"], Store,
@@ -27,10 +30,27 @@ fn main() {
         ap.parse_args_or_exit();
     }
 
+    fn d_check(r: Result<u32, std::num::ParseIntError>) -> Result<u32, std::num::ParseIntError> {
+        if r.is_ok() {
+            return r;
+        }
+        else {
+            println!("Dimensions provided are not in the correct format.");
+            std::process::exit(1);
+        }
+    }
+
     if !d_temp.is_empty() {
         dimensions = d_temp.split('x').map(
-            |s| s.parse().unwrap()
+            |s| d_check(s.parse::<u32>()).unwrap()
         ).collect();
+
+        if dimensions.len() != 2 {
+            println!("Dimensions provided are not in the correct format.");
+            std::process::exit(1);
+        }
+
+        do_resize = true;
     }
 
     if verbose {
@@ -38,4 +58,14 @@ fn main() {
     }
     println!("it didnt crash");
     println!("{}\n{:?}", input, dimensions);
+    println!("image time yay");
+
+    let mut img = image::open(input).unwrap();
+    if do_resize {
+        img = img.resize_exact(dimensions[0], dimensions[1], image::imageops::FilterType::Lanczos3)
+    }
+
+    println!("if you made it this far then it read the image woah");
+
+    img.save(output).ok();
 }
